8f5c2ba0c9d2371bbf72314ba01633e55ab60de3
seungchul <ssc166@naver.com>
2022-10-27 21:34:13 +0900

    cmg test

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ssc/cmg_test/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ssc/cmg_test/devel;/home/ssc/KIMM/devel;/home/ssc/CULR_gazebo/devel;/home/ssc/cmg_roll/devel;/opt/ros/noetic'.split(';') if '/home/ssc/cmg_test/devel;/home/ssc/KIMM/devel;/home/ssc/CULR_gazebo/devel;/home/ssc/cmg_roll/devel;/opt/ros/noetic' != '' else []
--- build/cmg_test_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ssc/cmg_test/src/cmg_test_description
-# Build directory: /home/ssc/cmg_test/build/cmg_test_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
